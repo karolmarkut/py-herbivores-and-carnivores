332251cdc0cc2946e44eca2c407ba360be7bac73
Karol <karolmarkut11@gmail.com>
2026-02-05 09:26:40 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -3,13 +3,18 @@ from __future__ import annotations
 class Animal:
     alive: list[Animal] = []
 
-    def __init__(self, name: str) -> None:
+    def __init__(
+            self,
+            name: str
+    ) -> None:
         self.name: str = name
         self.health: int = 100
         self.hidden: bool = False
         Animal.alive.append(self)
 
-    def _check_death(self) -> None:
+    def _check_death(
+            self,
+    ) -> None:
         if self.health <= 0:
             self.health = 0
             if self in Animal.alive:
@@ -19,11 +24,18 @@ class Animal:
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
 class Herbivore(Animal):
-    def hide(self) -> None:
+    class Herbivore(Animal):
+        def hide(
+                self,
+        ) -> None:
         self.hidden = not self.hidden
 
 class Carnivore(Animal):
-    def bite(self, target: Animal) -> None:
+    def bite(
+            self,
+            target: Animal
+    ) -> None:
         if isinstance(target, Herbivore) and not target.hidden:
             target.health -= 50
             target._check_death()
+
